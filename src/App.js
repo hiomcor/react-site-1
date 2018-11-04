e2ddd0d154b2284ef099026a2db3bc2b8ab0b41a
hiomcor <coreyhiom@gmail.com>
2018-11-04 22:21:51 +0000

    General Progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
 
-
+let fakeServerData = {
+  user: {
+    name: 'Corey'
+  }
+}
 
 class NavbarBootstrap extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="/home">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
@@ -43,34 +47,20 @@ class NavbarBootstrap extends Component {
   };
 }
 
-class Carousel extends Component {
+class Banner extends Component {
+  constructor() {
+    super();
+    this.state = { serverData: {} }
+  }
+  componentDidMount() {
+    this.setState({ serverData: fakeServerData })
+  }
   render() {
     return (
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=777&fg=555&text=First slide" alt="First slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide" />
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src=".../800x400?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide" />
-          </div>
+      <div className="body-pad">
+        <div className="banner-home d-flex justify-content-center align-items-center">
+          <h1>{this.state.serverData.user && this.state.serverData.user.name}'s Test Site</h1>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
       </div>
     );
   }
@@ -102,7 +92,11 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-
+        <div className="container pt-5">
+          <img src="images/JB-circle.svg" alt="JB Logo" />
+          <h2>Lets work<br />together</h2>
+          <img src="images/right-arrow-large.svg" alt="arrow" />
+        </div>
       </footer>
     );
   }
@@ -113,6 +107,7 @@ class App extends Component {
     return (
       <div className="App">
         <NavbarBootstrap />
+        <Banner />
         {/* <Carousel /> */}
         <Staggered />
         <Footer />
