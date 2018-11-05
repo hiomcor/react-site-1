6a22055a7a199d999ae4291eb2a46e33acfd5cda
hiomcor <coreyhiom@gmail.com>
2018-11-05 01:08:27 +0000

    Icon animation

--- src/App.js
+++ src/App.js
@@ -10,36 +10,36 @@ let fakeServerData = {
 class NavbarBootstrap extends Component {
   render() {
     return (
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/home">Navbar</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/home">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/home">Home <span className="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Link</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="/action">Action</a>
-                <a class="dropdown-item" href="/another-action">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="/something-else">Something else here</a>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="/action">Action</a>
+                <a className="dropdown-item" href="/another-action">Another action</a>
+                <div className="dropdown-divider"></div>
+                <a className="dropdown-item" href="/something-else">Something else here</a>
               </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="/disabled">Disabled</a>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="/disabled">Disabled</a>
             </li>
           </ul>
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
           </form>
         </div>
       </nav>
@@ -93,9 +93,10 @@ class Footer extends Component {
     return (
       <footer>
         <div className="container pt-5">
-          <img src="images/JB-circle.svg" alt="JB Logo" />
+          <img className="svg-rotate" src={("/images/JB-circle.svg")} alt="JB Logo" width="25" />
           <h2>Lets work<br />together</h2>
-          <img src="images/right-arrow-large.svg" alt="arrow" />
+          <img src={("/images/right-arrow-large.svg")} alt="arrow" height="16" />
+          <div className="shapeshifter" id='btn'></div>
         </div>
       </footer>
     );
